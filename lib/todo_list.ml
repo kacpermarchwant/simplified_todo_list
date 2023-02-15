81cc12d224989bc36e32670b586fc0e0a48ab0f7
Kacper Marchwant <kacper.marchwant@gmail.com>
2023-02-15 23:11:25 +0100

    remove unnecessary comment

--- lib/todo_list.ml
+++ lib/todo_list.ml
@@ -1,13 +1,5 @@
 open Types
 
-(* Can I turn it into a functor?
-
-   Store can be a parameter - different implementation - Bruteforce and InvertedIndex
-
-   Functors are functors from modules to modules
-
-   Functors let you create parametrized modules and then provide other modules as parameters to get specific implementation *)
-
 type t = { store : Store.t }
 
 let add (todo_list : t) (add_params : add_params) : todo_item =
