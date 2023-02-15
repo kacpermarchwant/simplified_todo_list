65636f16736483978988e3e258c0a6919b90f3b8
Kacper Marchwant <kacper.marchwant@gmail.com>
2023-02-15 23:10:46 +0100

    make parser more readable

--- lib/parser.ml
+++ lib/parser.ml
@@ -4,6 +4,9 @@ open Types
 
 type search_expr_element = WordEl of search_word | TagEl of search_tag
 
+let serialize_tag tag = TagEl (SearchTag tag)
+let serialize_word word = WordEl (SearchWord word)
+
 let separate_words_from_tags search_query : search_word list * search_tag list =
   List.fold_left search_query ~init:([], []) ~f:(fun (words, tags) el ->
       match el with
@@ -20,13 +23,15 @@ let is_lowercase_or_dash_or_whitespace (c : char) : bool =
   is_lowercase_or_dash c || is_whitespace c
 
 let is_digit (c : char) : bool = Char.is_digit c
+
+let take_one_or_more_whitespace =
+  satisfy is_whitespace *> skip_while is_whitespace
+
 let take_word = take_while1 is_lowercase_or_dash
 let take_tag = char '#' *> take_word
 
 let take_tags =
-  many
-    (satisfy is_whitespace *> skip_while is_whitespace
-    *> (take_tag >>| fun tag -> Tag tag))
+  many (take_one_or_more_whitespace *> (take_tag >>| fun tag -> Tag tag))
 
 let take_description =
   skip_while is_whitespace *> char '\"'
@@ -38,16 +43,14 @@ let parse_add : (description * tag list) t =
   string "add" *> both take_description take_tags <* skip_while is_whitespace
 
 let parse_done : string t =
-  string "done" *> char ' ' *> skip_while is_whitespace *> take_while1 is_digit
+  string "done" *> take_one_or_more_whitespace *> take_while1 is_digit
   <* skip_while is_whitespace
 
 let parse_search : search_expr_element list t =
   string "search"
   *> many
-       (satisfy is_whitespace *> skip_while is_whitespace
-       *> (take_tag
-          >>| (fun tag -> TagEl (SearchTag tag))
-          <|> (take_word >>| fun word -> WordEl (SearchWord word))))
+       (take_one_or_more_whitespace
+       *> (take_tag >>| serialize_tag <|> (take_word >>| serialize_word)))
   <* skip_while is_whitespace
 
 let parse_query (expr : string) : query =
